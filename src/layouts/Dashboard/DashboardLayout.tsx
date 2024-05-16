1ded3f364249ab686d112bce4573473a99a0b85c
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-05-16 17:25:15 +0600

    add: dashboard layout

---
+++ src/layouts/Dashboard/DashboardLayout.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const DashboardLayout: FC = () => {
+    return (
+        <div>
+            DashboardLayout
+        </div>
+    );
+};
+
+export default DashboardLayout;
